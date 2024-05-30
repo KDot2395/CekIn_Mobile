32f479df81821907683fbab430838250a7b6fd11
Micin23 <gregoriusmarcelino7@gmail.com>
2024-05-30 18:44:28 +0700

    initial push

---
+++ lib/terms.dart/terms.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TermsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Terms and Conditions'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Text(
+          'Here are the terms and conditions...',
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
